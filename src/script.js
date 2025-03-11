b9cb806c9a82d3680506e28232cc8e5314a1118a
Nastija <nastija.sij@gmail.com>
2025-03-11 12:15:14 +0100

    added axios

--- src/script.js
+++ src/script.js
@@ -1,3 +1,5 @@
+console.log("Axios Object:", axios);
+
 function search(event) {
   event.preventDefault();
   let searchInputElement = document.querySelector("#search-input");
@@ -7,3 +9,16 @@ function search(event) {
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", search);
+
+function displayTemperature(response) {
+  console.log(response.data);
+}
+console.log("Checking if Axios is available:", typeof axios);
+
+let city = "Vienna";
+let apiKey = "o1cfacc0a9aa5038bce80c403dc4abt1";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+
+console.log("Axios Object:", axios);
+
+axios.get(apiUrl);
