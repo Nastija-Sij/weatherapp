44009967b94e57750846818b3d060893544c7371
Nastija <nastija.sij@gmail.com>
2025-03-11 19:59:13 +0100

    JS update with city and current info-working

--- src/script.js
+++ src/script.js
@@ -3,21 +3,16 @@ function search(event) {
   let searchInputElement = document.querySelector("#search-input");
   let cityElement = document.querySelector("#current-city");
   cityElement.innerHTML = searchInputElement.value;
-}
 
-let searchForm = document.querySelector("#search-form");
-searchForm.addEventListener("submit", search);
-
-function searchCity(city) {
   let apiKey = "o1cfacc0a9aa5038bce80c403dc4abt1";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${searchInputElement.value}&key=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayTemperature);
-}
+} //* This way, when you search for a city, the API will actually be called.
 
 function formatDate(date) {
-  let minutes = now.getMinutes();
-  let hours = now.getHours();
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
   if (hours < 10) {
     hours = `0${hours}`;
   }
@@ -33,25 +28,44 @@ function formatDate(date) {
     "Friday",
     "Saturday",
   ];
-  let day = days[now.getDay()];
-  return `${day} ${hours}:${minutes}`;
+  let day = days[date.getDay()];
+
+  return `${day}, ${date.getDate()}.${
+    date.getMonth() + 1
+  }.${date.getFullYear()}, ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
+  console.log(response.data); // debugging to check the API response
   let temperatureElement = document.querySelector("#current-temperature");
   let headingElement = document.querySelector("#current-city");
-  let descriptionElement = document.querySelector("#date-time-paragraph");
+  let descriptionElement = document.querySelector("#weather-description");
   let physicsElement = document.querySelector("#physics");
-  let iconUrlElement = document.querySelector("#current-icon");
+  let iconElement = document.querySelector("#current-icon");
+  let dateElement = document.querySelector("#date-time-paragraph");
 
   let temperature = Math.round(response.data.temperature.current);
   let city = response.data.city;
-  let description = response.data.conditions.description;
+  let description = response.data.condition.description;
   let humidity = response.data.temperature.humidity;
   let wind = Math.round(response.data.wind.speed);
-  let iconUrl = response.data.condition.icon;
+  let iconUrl = response.data.condition.icon_url;
 
   temperatureElement.innerHTML = `${temperature}`;
   headingElement.innerHTML = `${city}`;
-  descriptionElement.innerHTML = 
+  descriptionElement.innerHTML = `${description} with a temperature of`;
+  physicsElement.innerHTML = `Humidity: ${humidity}% <br /> Wind: ${wind} km/h`;
+  iconElement.innerHTML = `<img src="${iconUrl}" alt="${description}" width="90px"/>`;
+  dateElement.innerHTML = formatDate(new Date());
 }
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", search);
+
+let currentDateEelement = document.querySelector("#current-date");
+
+//Load vienna by default
+let apiKey = "o1cfacc0a9aa5038bce80c403dc4abt1";
+let apiUrl = `https://api.shecodes.io/weather/v1/current?query=Vienna&key=${apiKey}&units=metric`;
+
+axios.get(apiUrl).then(displayTemperature);
